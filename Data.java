27b2f7406b2d4bea658fe2f56fbf81dc75df9545
Fathima Shehna <fathimashehna876@gmail.com>
2026-02-13 14:20:40 +0530

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,11 @@
+public class Data {
+    void getdata(int a)
+    {
+        System.out.println("A="+a);
+    }
+    public static void main(String arrgs[])
+    {
+        Data ob=new Data();
+        ob.getdata(10);
+    }
+}
