86595cb264022e7a7b306983a2ce34d5e1732290
Fathima Shehna <fathimashehna876@gmail.com>
2026-02-13 14:19:02 +0530

    Add files via upload

---
+++ obj.java
@@ -0,0 +1,11 @@
+public class obj {
+    void msg()
+    {
+        System.out.println("Classes & objects");
+    }
+    public static void main(String args[])
+    {
+        obj ob=new obj();
+        ob.msg();
+    }
+}
