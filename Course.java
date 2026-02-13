6b43a022b4e223d969e4a2893a22263f2f7dde65
Fathima Shehna <fathimashehna876@gmail.com>
2026-02-13 14:20:16 +0530

    Add files via upload

---
+++ Course.java
@@ -0,0 +1,10 @@
+public class Course
+{
+    public static void main(String args[])
+    {
+        int rollno=1723;
+        String course="MCA";
+        System.out.println("Roll no:"+rollno);
+        System.out.println("Course:"+course);
+    }
+}
